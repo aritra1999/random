9dc9c8a2c23396f55705a12e1f312294e8c39a8d
Aritra Mondal <aritra041999.dev@gmail.com>
2023-03-03 10:28:19 +0530

    Added search functionality

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,10 +2,11 @@
     import "../app.css";
     import Navbar from "$lib/components/Navbar.svelte";
 	import Footer from "$lib/components/Footer.svelte";
+
 </script>
 
-<Navbar />
 <div class="container py-4 px-2 mx-auto">
+    <Navbar />
     <slot />
 </div> 
 <Footer />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,29 @@
-<script> 
-    import List from "$lib/components/List.svelte";
+<script lang="ts"> 
+    import type { Item } from "$lib/types/types";
     import items from "$lib/data/collection.json";
+    import { searchStore } from "$lib/store/searchStore";
+
+    import List from "$lib/components/List.svelte";
+    
+    let itemList = items;
+    const SEARCH_THRESHOLD = 1;
+    
+    $: ($searchStore.length >= SEARCH_THRESHOLD) ? 
+        itemList = items.filter((item: Item) => {     
+            const searchTerm = $searchStore.toLocaleLowerCase();
+            return  item.title.toLowerCase().includes(searchTerm) || 
+                    item.icon?.toLowerCase().includes(searchTerm) || 
+                    item.tags?.join().toLowerCase().includes(searchTerm);
+        }) : itemList = items;
+    
 </script>
 
-<List items={items} />
+{#if $searchStore.length >= SEARCH_THRESHOLD}
+    <div class="p-2 mb-4 text-sm text-blue-600 rounded-lg " role="alert">
+        Found <span class="font-semibold">{itemList.length}</span> items, searching for <span class="font-semibold">{$searchStore}</span>
+    </div>
+{/if}
+
+<List items={itemList} />
+
 
