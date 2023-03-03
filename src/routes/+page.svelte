c13d42488b21a0f81432683900198d80d86935e6
Aritra Mondal <aritra041999.dev@gmail.com>
2023-03-03 22:26:50 +0530

    added remote json fetching before page load

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,29 +1,30 @@
 <script lang="ts"> 
     import type { Item } from "$lib/types/types";
-    import items from "$lib/data/collection.json";
     import { searchStore } from "$lib/store/searchStore";
 
     import List from "$lib/components/List.svelte";
     
-    let itemList = items;
     const SEARCH_THRESHOLD = 1;
     
+    export let data: {items: Item[]}; 
+    const ogData = data;
+
     $: ($searchStore.length >= SEARCH_THRESHOLD) ? 
-        itemList = items.filter((item: Item) => {     
+        data.items = data.items.filter((item: Item) => {     
             const searchTerm = $searchStore.toLocaleLowerCase();
             return  item.title.toLowerCase().includes(searchTerm) || 
                     item.icon?.toLowerCase().includes(searchTerm) || 
                     item.tags?.join().toLowerCase().includes(searchTerm);
-        }) : itemList = items;
+        }) : data = ogData;
     
 </script>
 
 {#if $searchStore.length >= SEARCH_THRESHOLD}
     <div class="p-2 mb-4 text-sm text-blue-600 rounded-lg " role="alert">
-        Found <span class="font-semibold">{itemList.length}</span> items, searching for <span class="font-semibold">{$searchStore}</span>
+        Found <span class="font-semibold">{data.items.length}</span> items, searching for <span class="font-semibold">{$searchStore}</span>
     </div>
 {/if}
 
-<List items={itemList} />
+<List items={data.items} />
 
 
