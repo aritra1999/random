9075a983b6abe05bf1cc395ed26d4cf4cc8b88f3
Aritra Mondal <aritra041999.dev@gmail.com>
2023-03-06 11:11:03 +0530

    updating source from `store` to `shelf`

--- src/routes/+page.ts
+++ src/routes/+page.ts
@@ -1,5 +1,5 @@
 export const load = async ({ fetch}) => {
-    const URL = "https://raw.githubusercontent.com/aritra1999/store/master/rand0m/collection.json";
+    const URL = "https://raw.githubusercontent.com/aritra1999/shelf/master/rand0m/collection.json";
     const response = await fetch(URL,{method:'GET'});
     const collection = await response.json();  
 
