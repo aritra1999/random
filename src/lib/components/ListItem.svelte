05b275318c4bd06f4d2a7d4c208a8da9de63b419
Aritra Mondal <aritra041999.dev@gmail.com>
2023-03-07 16:31:53 +0530

    added gist item

--- src/lib/components/ListItem.svelte
+++ src/lib/components/ListItem.svelte
@@ -1,10 +1,25 @@
 <script lang="ts">
 	import type { Item } from "$lib/types/types"; 
+	import { onMount } from "svelte";
+	import SvelteMarkdown from "svelte-markdown";
     export let item: Item;
+    onMount(async () => {
+        if(item.type === "gist") {
+            try {
+                item.description = await (await fetch(item.path, {method: 'GET'})).text();
+            } catch {
+                item.description = "Error loading gist"
+            }
+            item.path = "";
+        }
+
+        item.path = `${item.type === 'link' ? '' : item.type === 'blog' ? '/blog/' : '/app/'}${item.path}`;
+    });
+    
 </script> 
 
 
-<a href="{item.type === 'link' ? '' : item.type === 'blog' ? '/blog/' : '/app/'}{item.path}" target="_blank" rel="noreferrer" class="inline-block w-full px-6 py-3 rounded-lg font-semibold border-[3px] border-slate-100 hover:border-blue-600 hover:text-blue-600 hover:bg-blue-50 group mb-4">
+<a href={item.path} target="_blank" rel="noreferrer" class="inline-block w-full px-6 py-3 rounded-lg border-[3px] border-slate-100 hover:border-blue-600 hover:text-blue-600 hover:bg-blue-50 group mb-4">
     <div class="flex items-center space-x-2">
         {#if item.icon}
             <img src={item.icon} alt="" class="h-5 w-5"/>
@@ -18,17 +33,26 @@
                     🎮
                 {:else if item.type === "blog"}
                     📄
+                {:else if item.type === "gist"}
+                    📐
                 {:else}
                     ❓
                 {/if}
             </div>
         {/if}
         <div>
-            <span>{item.title}</span>
+            <span class="font-semibold">
+                {item.title}
+            </span>
+            <span class="tag">
+                {item.type}
+            </span>
         </div>
     </div>
     {#if item.description}
-        <div class="mt-2 text-sm text-gray-500">{item.description}</div>
+        <div class="my-1 font-medium text-sm">
+            <SvelteMarkdown source={item.description} />
+        </div>
     {/if}
     {#if item.tags}
         <div class="mt-2">
