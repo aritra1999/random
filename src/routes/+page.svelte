d375bb797112b4a4b335e235bf898b42c15721e6
Aritra Mondal <aritra041999.dev@gmail.com>
2023-03-07 15:39:27 +0530

    fixed search

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,38 @@
 <script> 
     import List from "$lib/components/List.svelte";
+<<<<<<< Updated upstream
     import items from "$lib/data/collection.json";
 </script>
 
 <List items={items} />
+=======
+	import type { PageData } from "./$types";
+    
+    export let data: PageData;
+    let items: Item[] = data.collection;
+    const ogItems = items;
+    const SEARCH_THRESHOLD = 1;
+
+    $: ($searchStore.length >= SEARCH_THRESHOLD) ? 
+        items = ogItems.filter((item: Item) => {     
+            const searchTerm = $searchStore.toLocaleLowerCase();
+            return  item.title.toLowerCase().includes(searchTerm) || 
+                    item.icon?.toLowerCase().includes(searchTerm) || 
+                    item.description?.toLowerCase().includes(searchTerm) ||
+                    item.tags?.join().toLowerCase().includes(searchTerm);
+        }) : items = ogItems;
+    
+</script>
+
+{#if $searchStore.length >= SEARCH_THRESHOLD}
+    <div class="p-2 mb-4 text-sm text-blue-600 rounded-lg " role="alert">
+        Found 
+        <span class="font-semibold">{items.length}</span> 
+        items, searching for 
+        <span class="font-semibold">{$searchStore}</span>
+    </div>
+{/if}
+<List items={items} />
+
+>>>>>>> Stashed changes
 
