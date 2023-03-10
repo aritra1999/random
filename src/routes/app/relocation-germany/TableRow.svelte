6aebb7569a2f8c684c2ac7d4181438b836911de1
Aritra Mondal <aritra041999.dev@gmail.com>
2023-03-10 11:53:56 +0530

    UI restructuring

---
+++ src/routes/app/relocation-germany/TableRow.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	import { eurFormatter, inrFormatter } from "./utils";
+
+    export let baseValue: number; 
+    export let conversionRate: number; 
+    export let bgColor: string = "bg-transparent"; 
+    export let isNegative: boolean = false; 
+
+</script>
+
+<td class="{bgColor}">
+    {#if isNegative }- {/if}{eurFormatter.format(baseValue)}
+</td>
+<td class={bgColor}>
+    {#if isNegative }- {/if}{eurFormatter.format(baseValue/12)}
+</td>
+<td class={bgColor}>
+    {#if isNegative }- {/if}{inrFormatter.format(baseValue*conversionRate)}
+</td>
+<td class={bgColor}>
+    {#if isNegative }- {/if}{inrFormatter.format((baseValue*conversionRate)/12)}
+</td>
