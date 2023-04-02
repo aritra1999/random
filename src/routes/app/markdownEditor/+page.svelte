6f3f1996ec816ef717e8467e3662ab2f155055cc
Aritra Mondal <aritra041999.dev@gmail.com>
2023-04-02 10:34:16 +0530

    Add control items at cursor location

--- src/routes/app/markdownEditor/+page.svelte
+++ src/routes/app/markdownEditor/+page.svelte
@@ -7,18 +7,17 @@
     function execute(control: { type: string; icon: string; content: string; }) {
         const editor = document.getElementById("editor") as HTMLInputElement;
         
-        const startPosition = editor.selectionStart;
-        const endPosition = editor.selectionEnd;
+        const startPosition = editor.selectionStart ?? 0;
+        const endPosition = editor.selectionEnd ?? 0;
 
-        
-    
+        input = input.slice(0, startPosition) + '\n' + control.content + input.slice(endPosition);
+        editor.focus();    
     }
 
 </script>
 <div class="flex w-full h-full">
     <div class="w-1/2 h-[calc(100vh-10rem)] border-r border-slate-200">
         <div class="flex divider-2 divider-slate-200">
-            <!-- loop over each control and display them -->
             {#each controls as control}
                 <button class="p-2 font-extralight" on:click={() => execute(control)}>
                     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" stroke-width="0.5" stroke="currentColor" class="w-4 h-4">
