41c685d9a47dc8349eb8c02ce4ef29166552e06e
Aritra Mondal <aritra041999.dev@gmail.com>
2023-04-18 22:01:49 +0530

    Set navbar width to container width

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -2,18 +2,20 @@
     import { page } from '$app/stores';
     import { searchStore } from '$lib/store/searchStore';
 </script>  
-<nav class="flex items-center justify-between px-2 h-16 mb-4 backdrop-blur-sm bg-white/50 border-[2px] border-slate-100 rounded-lg fixed w-screen">
-  <a href="/" class="flex items-center ml-2">
-    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-3">
-      <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z" />
-    </svg>      
-    <span class="self-center whitespace-nowrap text-xl font-extrabold dark:text-white hidden sm:block">
-      rand0m
-    </span>
-  </a>
-  {#if $page.url.pathname === '/'}
-    <div class="flex md:order-2">
-      <input bind:value={$searchStore} id="search-navbar" class="flex items-center justify-between px-4 py-2 bg-slate-100 rounded-lg border-[2px] border-slate-100 hover:border-black active:border-black" placeholder="Search..." />
-    </div>
-  {/if}
+<nav class="mb-4 backdrop-blur-sm bg-white/50 border-[2px] border-slate-100 rounded-lg fixed w-screen">
+  <div class="container mx-auto flex items-center justify-between px-2 h-16">
+    <a href="/" class="flex items-center ml-2">
+      <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 mr-3">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M16.5 6v.75m0 3v.75m0 3v.75m0 3V18m-9-5.25h5.25M7.5 15h3M3.375 5.25c-.621 0-1.125.504-1.125 1.125v3.026a2.999 2.999 0 010 5.198v3.026c0 .621.504 1.125 1.125 1.125h17.25c.621 0 1.125-.504 1.125-1.125v-3.026a2.999 2.999 0 010-5.198V6.375c0-.621-.504-1.125-1.125-1.125H3.375z" />
+      </svg>      
+      <span class="self-center whitespace-nowrap text-xl font-extrabold dark:text-white hidden sm:block">
+        rand0m
+      </span>
+    </a>
+    {#if $page.url.pathname === '/'}
+      <div class="flex md:order-2">
+        <input bind:value={$searchStore} id="search-navbar" class="flex items-center justify-between px-4 py-2 bg-slate-100 rounded-lg border-[2px] border-slate-100 hover:border-black active:border-black" placeholder="Search..." />
+      </div>
+    {/if}
+  </div>
 </nav>
