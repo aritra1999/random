8b6d41efbcd42932e409c18cd4fff966b45af550
Aritra Mondal <aritra041999.dev@gmail.com>
2023-04-22 01:06:57 +0530

    Moved search from UI to home page

--- src/lib/components/Navbar.svelte
+++ src/lib/components/Navbar.svelte
@@ -1,7 +1,3 @@
-<script lang="ts">
-    import { page } from '$app/stores';
-    import { searchStore } from '$lib/store/store';
-</script>  
 <nav class="mb-4 backdrop-blur-sm bg-white/50 border-[2px] border-slate-100 rounded-lg fixed w-screen">
   <div class="container mx-auto flex items-center justify-between px-2 h-16">
     <a href="/" class="flex items-center ml-2">
@@ -12,10 +8,5 @@
         rand0m
       </span>
     </a>
-    {#if $page.url.pathname === '/'}
-      <div class="flex md:order-2">
-        <input bind:value={$searchStore} id="search-navbar" class="flex items-center justify-between px-4 py-2 bg-slate-100 rounded-lg border-[2px] border-slate-100 hover:border-black active:border-black" placeholder="Search..." />
-      </div>
-    {/if}
   </div>
 </nav>
---
+++ src/lib/components/Search.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+    import { searchStore } from '$lib/store/store';
+</script>
+
+<div class="flex items-center justify-between px-4 py-2 bg-white rounded-lg border-[2px] border-slate-100 hover:border-black focus:border-black space-x-3 w-full sm:w-96">
+    <input bind:value={$searchStore} class="" placeholder="Search..." />
+    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 text-slate-400">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
+    </svg>      
+</div>
