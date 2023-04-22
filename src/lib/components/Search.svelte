b0d3106201747613536f1b137ba2b24ffd949568
Aritra Mondal <aritra041999.dev@gmail.com>
2023-04-22 11:02:44 +0530

    Search icon

--- src/lib/components/Search.svelte
+++ src/lib/components/Search.svelte
@@ -16,7 +16,7 @@
 {#if showSearch}
     <div class="fixed top-0 left-0 w-screen h-16 backdrop-blur-sm bg-white/50 flex items-center justify-between px-6">
         <div class="flex items-center space-x-4">
-            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5">
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="2" stroke="currentColor" class="w-5 h-5 text-slate-700">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M21 21l-5.197-5.197m0 0A7.5 7.5 0 105.196 5.196a7.5 7.5 0 0010.607 10.607z" />
             </svg>      
             <input bind:value={$searchStore} use:init class="bg-transparent w-full text-slate-800" placeholder="Search..." />
